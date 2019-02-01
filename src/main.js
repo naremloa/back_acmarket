3517b1460631c6eb7ece31a9e17cbd7dbd0dff48
lucy <qwer558a1@gmail.com>
2019-02-01 14:33:29 +0800

    Revert "update"
    This reverts commit 1e5500576e88f33c3c35bb53146c0a627b9e04e2.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 import Vue from 'vue';
 import './plugins/vuetify';
-
-import 'roboto-fontface/css/roboto/roboto-fontface.css';
-import '@mdi/font/css/materialdesignicons.css';
-import '@/theme/index.styl';
-
 import App from './App.vue';
 import router from './router';
 import store from './store';
 // import './registerServiceWorker';
 import '@/permission';
+
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import '@mdi/font/css/materialdesignicons.css';
+import '@/theme/index.styl';
 
 Vue.config.productionTip = false;
 
