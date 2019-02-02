91bcf17711cb7005f7476bbd49613e63b1e509b9
lucy <qwer558a1@gmail.com>
2019-02-02 16:26:33 +0800

    move App to last

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 
 import Vue from 'vue';
 import './plugins/vuetify';
-import App from './App.vue';
 import router from './router';
 import store from './store';
 // import './registerServiceWorker';
@@ -10,6 +9,7 @@ import '@/permission';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
 import '@/theme/index.styl';
+import App from './App.vue';
 
 Vue.config.productionTip = false;
 
