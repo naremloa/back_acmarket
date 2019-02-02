1e34208a5febf585b3979b75a726fa5b8c574b5d
naremloa <naremloa@gmail.com>
2019-02-03 05:04:06 +0800

    update

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import '@/permission';
 
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
+import 'vuetify/src/stylus/app.styl';
 import '@/theme/index.styl';
 
 Vue.config.productionTip = false;
