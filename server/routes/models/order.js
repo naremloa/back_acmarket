666fea3bcdb6782694fe18eb341f95f3c97b9502
naremloa <naremloa@gmail.com>
2019-02-03 02:17:54 +0800

    update

---
+++ server/routes/models/order.js
@@ -0,0 +1,24 @@
+import { Models } from '../../db';
+
+const { Order } = Models;
+
+const orderFind = async (query) => {
+  const res = await Order.find(query);
+  return res;
+};
+
+const orderCount = async () => {
+  const res = await Order.estimatedDocumentCount();
+  return res;
+};
+
+const orderInsert = async (orderObj) => {
+  const res = await Order.create(orderObj);
+  return res;
+};
+
+export {
+  orderFind,
+  orderCount,
+  orderInsert,
+};
