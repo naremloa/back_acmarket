ddce18290ebcaf90ec2559c0489f6366eee7f6d5
naremloa <naremloa@gmail.com>
2019-02-11 11:18:44 +0800

    update

---
+++ src/components/editor.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="editor">
+    <div ref="editor" />
+  </div>
+</template>
+<script>
+import _Quill from 'quill';
+
+const Quill = window.Quill || _Quill;
+
+export default {
+  name: 'editor',
+  beforeDestroy() {
+    this.quill = null;
+    delete this.quill;
+  },
+  data() {
+    return {
+      options: {
+        theme: 'snow',
+        placeholder: '請輸入......',
+        formats: ['bold', 'header'],
+      },
+    };
+  },
+  mounted() {
+    this.quill = new Quill(this.$refs.editor, this.options);
+  },
+};
+</script>
--- src/views/pages/article/articleAdd.vue
+++ src/views/pages/article/articleAdd.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="article-add">
     <div>新增文章</div>
+    <editor />
   </div>
 </template>
 <script>
+import editor from '@/components/editor.vue';
+import 'quill/dist/quill.snow.css';
+
 export default {
   name: 'articleAdd',
+  components: {
+    editor,
+  },
 };
 </script>
