7b6d6f6a945cc76de8de06a98255e9e66834b276
naremloa <naremloa@gmail.com>
2019-02-12 18:24:31 +0800

    update

---
+++ src/components/searchGroup.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="search-group">
+    <v-form v-model="valid" class="px-2">
+      <v-layout row wrap>
+      </v-layout>
+    </v-form>
+  </div>
+</template>
+<script>
+export default {
+  name: 'searchGroup',
+  data() {
+    return {
+      valid: false,
+    };
+  },
+};
+</script>
---
+++ src/components/shInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-flex sm12 md4 lg3 px-1>
+    <v-text-field
+      v-model="value"
+      :label="setting.label || ''"
+      clearable />
+  </v-flex>
+</template>
+<script>
+export default {
+  name: 'sh-input',
+  props: {
+    propValue: {
+      required: true,
+    },
+    setting: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  model: {
+    prop: 'propValue',
+    event: 'valueChange',
+  },
+  data() {
+    return {
+      value: '',
+    };
+  },
+  watch: {
+    value(val) { if (val !== this.propValue) this.$emit('valueChange', val); },
+    propValue(val) { if (val !== this.value) this.value = this.propValue; },
+  },
+};
+</script>
