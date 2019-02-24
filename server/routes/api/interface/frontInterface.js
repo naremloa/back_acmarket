bf316f97b7669fb8993bf142a442a39f015a9444
naremloa <naremloa@gmail.com>
2019-02-24 23:23:42 +0800

    update

--- server/routes/api/interface/frontInterface.js
+++ server/routes/api/interface/frontInterface.js
@@ -1,10 +1,44 @@
 import {
   getLocation,
 } from '../location';
+import {
+  addOrder,
+} from '../order';
+import {
+  getOccList,
+} from '../occ';
 
 export const get = [
   ['/front/location/get',
     getLocation],
+
+  /**
+   * @api {get} /front/occ/list 佔用表
+   * @apiPermission none
+   * @apiName occList
+   * @apiGroup Front
+   *
+   * @apiSuccessExample {json} Success-Response:
+   * {
+   *    "occ": {
+   *      "20190226": { A: 2, B: 3, C: 0 },
+   *      "20190227": { A: 0, B: 1, C: 2 },
+   *      "20190228": { A: 3, B: 1, C: 2 },
+   *    },
+   *    "info": {
+   *      "A": 5,
+   *      "B": 3,
+   *      "C": 4,
+   *    }
+   * }
+   */
+  ['/front/occ/list',
+    getOccList],
+];
+
+export const post = [
+  ['/front/order/new',
+    addOrder],
 ];
 
 // export const post = [
