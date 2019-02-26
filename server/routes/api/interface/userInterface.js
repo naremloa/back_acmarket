2be88b51bbfd6348c75194ed37d94cc908e6b649
naremloa <naremloa@gmail.com>
2019-02-26 17:47:53 +0800

    update

--- server/routes/api/interface/userInterface.js
+++ server/routes/api/interface/userInterface.js
@@ -26,64 +26,73 @@ export const get = [
 
 export const post = [
   /**
- * @api {post} /user/review 審核帳號接口
- * @apiPermission login
- * @apiName userReview
- * @apiGroup User
- *
- * @apiParam {Number=1,3} status 審核狀態, 1為通過, 3為不通過
- * @apiParam {Number} role 角色id
- * @apiParam {String} id 帳號objectId
- * @apiParamExample {json} Request-example:
- * # 通過
- * {
- *    "status": "1",
- *    "role": "1000",
- *    "id": "5c49e2ac363210458eb621dc"
- * }
- *
- * # 不通過
- * {
- *    "status": 3,
- *    "id": "5c49e2ac363210458eb621dc"
- * }
- *
- */
+   * @api {post} /user/review 審核帳號接口
+   * @apiPermission login
+   * @apiName userReview
+   * @apiGroup User
+   *
+   * @apiParam {Number=1,3} status 審核狀態, 1為通過, 3為不通過
+   * @apiParam {Number} role 角色id
+   * @apiParam {String} id 帳號objectId
+   * @apiParamExample {json} Request-example:
+   * # 通過
+   * {
+   *    "status": "1",
+   *    "role": "1000",
+   *    "id": "5c49e2ac363210458eb621dc"
+   * }
+   *
+   * # 不通過
+   * {
+   *    "status": 3,
+   *    "id": "5c49e2ac363210458eb621dc"
+   * }
+   *
+   */
   ['/user/review',
     middlewareCheckLoginStatusSession, middlewareCheckAuthorization,
     updateUserStatus],
 
   /**
- * @api {post} /user/update/role 帳號角色變更接口
- * @apiPermission login admin
- * @apiName userUpdateRole
- * @apiGroup User
- *
- * @apiParam {Number} role 角色id
- * @apiParam {String} cid 帳號objectId
- * @apiParamExample {json} Request-example:
- * {
- *    "role": "1000",
- *    "cid": "5c49e2ac363210458eb621dc"
- * }
- */
+   * @api {post} /user/update/role 帳號角色變更接口
+   * @apiPermission login admin
+   * @apiName userUpdateRole
+   * @apiGroup User
+   *
+   * @apiParam {Number} role 角色id
+   * @apiParam {String} cid 帳號objectId
+   * @apiParamExample {json} Request-example:
+   * {
+   *    "role": "1000",
+   *    "cid": "5c49e2ac363210458eb621dc"
+   * }
+   */
   ['/user/udpate/role',
     middlewareCheckLoginStatusSession, middlewareCheckAuthorization,
     updateUserRole],
 
   /**
- * @api {post} /user/role/add 角色添加接口
- * @apiPermission login admin
- * @apiName userRoleAdd
- * @apiGroup User
- *
- * @apiParam {String} name 角色名稱
- * @apiParam {Array} routerGroup 角色有權路由組
- */
+   * @api {post} /user/role/add 角色添加接口
+   * @apiPermission login admin
+   * @apiName userRoleAdd
+   * @apiGroup User
+   *
+   * @apiParam {String} name 角色名稱
+   * @apiParam {Array} routerGroup 角色有權路由組
+   */
   ['/user/role/add',
     middlewareCheckLoginStatusSession, middlewareCheckAuthorization,
     addRole],
 
+  /**
+   * @api {post} /user/role/update 角色修改接口
+   * @apiPermission login admin
+   * @apiName userRoleUpdate
+   * @apiGroup User
+   *
+   * @apiParam {String} cid 角色cid
+   * @apiParam {Array}  routerGroup 角色權限陣列
+   */
   ['/user/role/update',
     middlewareCheckLoginStatusSession, middlewareCheckAuthorization,
     updateRole],
